db67e454c3fc284030cd0c6a72bf54dd18ad2a54
Your Name <yeshwanthvenkat.u@gmail.com>
2025-11-01 22:11:11 +0530

    fixed

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 
 import { HydrateClient } from "~/trpc/server";
-import Sec1 from "./sec1";
+import Sec1 from "./hero";
 
 import Navbar from "../_components/navbar";
 
 import Footer from "../_components/footer";
 import Ourmission from "./ourmission";
-import Sec7 from "../_components/quality";
+import Quality from "../_components/quality";
 import Companies from "../_components/licenses";
 import Contact from "../_components/contact";
 
@@ -30,7 +30,7 @@ export default async function Home() {
                 <Sec1/>
        </section>
        <section><Ourmission/></section>
-       <section><Sec7/></section>
+       <section><Quality /></section>
        <section className="flex w-full justify-center mt-15 mb-10 items-center p-4"><Companies/></section>
        <section><Contact/></section>
        <section><Footer/></section>
