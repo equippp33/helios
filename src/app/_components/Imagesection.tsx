17c154eb3a1a556d46071c77521bb90e9f7f8689
Your Name <yeshwanthvenkat.u@gmail.com>
2025-11-04 10:20:04 +0530

    fixed

--- src/app/_components/Imagesection.tsx
+++ src/app/_components/Imagesection.tsx
@@ -1,21 +1,27 @@
-import Image from 'next/image';
+import Image from 'next/image'
 import React from 'react'
 
 const Imagesection = () => {
   return (
-    <div className='w-full '>
-      <div className='w-full h-fit'>
-        <h1 className='text-xl w-fit md:text-6xl font-extrabold bg-url animate-infiniteScroll2'>Helios</h1>
-        <Image 
-          src='/images/helioshero1.svg' 
-          alt='' width={400} height={300} 
-          className='w-full  top-0 z-50 md:h-[831px] object-cover h-[250px]'>
+    <div className="relative w-full">
+      {/* Helios sliding text section */}
+      <div className="absolute top-40 left-0 w-full z-10 pointer-events-none">
+        <h1 className="text-xl md:text-8xl w-fit font-extrabold animate-infiniteScroll2 text-black bg-clip-text ">
+          Helios
+        </h1>
+      </div>
 
-        </Image>
-
+      {/* Image section */}
+      <div className="w-full h-fit relative top-0 z-20">
+        <Image
+          src="/images/helioshero1.svg"
+          alt="Helios Hero"
+          width={400}
+          height={300}
+          className="w-full md:h-[821px] h-[250px] object-cover"
+        />
       </div>
     </div>
-    
   )
 }
 
